1a574526dffb82435f42efe3084addfb38c89e96
ronitloke <82684588+ronitloke@users.noreply.github.com>
2022-07-24 02:19:08 +0530

    Add files via upload

---
+++ Qspider/Looping statements/While loop/factorial.java
@@ -0,0 +1,15 @@
+class p5 
+{
+	public static void main(String[] args) 
+	{
+		int i = 5;
+		int fact = 1;
+
+		while (i>=1)
+		{
+			fact = fact * i;
+			--i;
+		}
+		System.out.println(fact);
+	}
+}
