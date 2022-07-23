a14b3dd4272754f56e5edcbcfaef2fdbd83c381c
ronitloke <82684588+ronitloke@users.noreply.github.com>
2022-07-23 23:28:15 +0530

    Add files via upload

--- Apna college/Lec 4/Even_num.java
+++ Apna college/Lec 4/Even_num.java
@@ -1,21 +1,32 @@
-import java.util.*;
-class p1 
+class p1
 {
 	public static void main(String[] args) 
 	{
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter a number");
-		
-		int n = sc.nextInt();
-		System.out.println("Even numbers are:");
-
-		for(int i=0; i<=n; i++)
-		{
-			if(i%2==0)
+		int i=0;
+		int n= 101;
+	
+       for (i=0;i<n;i++)
+        {
+			if (i%2==0)
 			{
-		System.out.println(i);
+		     System.out.println(i);
 			}
 		}
-
+		
+		while (i<n)
+		{
+			if (i%2==0)
+			{
+			System.out.println(i);
+			}
+			i++;
+		}
+		do
+		{
+		System.out.println(i);
+		i++;
+		}
+		while (i<n);
 	}
 }
+
